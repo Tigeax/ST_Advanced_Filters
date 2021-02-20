9865c96a259164dd76623b0dedb135015c0f060b
Jesse <jessebraaksma@gmail.com>
2021-02-20 22:31:41 +0100

    Setup for assignment 1

---
+++ 1_kalman.py
@@ -0,0 +1,26 @@
+"""
+Hanze University of Applied Sciences
+Electrical and Electronic Engineering - Major Sensor Technolgyy
+Advanced Filters
+Assignment #1 - Kalman Filter
+By Jesse Braaksma
+20/02/2011
+"""
+
+
+def __init__(self, timeStep, duration, gpsNoise, acceleration, accelerationNoise):
+    self.timeStep = timeStep
+    self.duration = duration
+    self.gpsNoise = gpsNoise
+    self.acceleration = acceleration
+    self.accelerationNoise = accelerationNoise
+    self.runSimulation()
+
+
+def runSimulation(self):
+    pass
+
+    
+
+
+
