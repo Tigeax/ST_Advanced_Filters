c8578b6bed46d58ab25c0ba3c3f8a68a46f1c212
Jesse Braaksma <jessebraaksma@gmail.com>
2021-04-20 14:08:54 +0200

    Updated assignment #1

--- 1-kalman_simple/simpleKalmanFilter.py
+++ 1-kalman_simple/simpleKalmanFilter.py
@@ -11,15 +11,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# File path to the csv files
 dataFilePath = "1-kalman_simple/data.csv"
 resultsFilePath = "1-kalman_simple/results.csv"
 
-# Get the simulated data from the csv file as a pandas dataframe
-dataDF = pd.read_csv(dataFilePath, names=['data', 'given_kf'], header=1)
-
+# Get the simulated data from the csv file as a pandas dataframe, only use the first 400 rows as data after this is incorrect.
+dataDF = pd.read_csv(dataFilePath, names=['data', 'given_kf'], header=0, nrows=400)
 
 T = 0.1 # Measurements are at 10Hz, so an interval of 0.1 seconds
+
+uk = 1 # The acceleration
 
 
 # State and output equations matrixes
@@ -33,9 +34,6 @@ C = np.matrix([[1,
                 0]])
 
 
-uk = 1 # The acceleration
-
-
 # Noise covarience matrixes
 Sw = np.matrix([[0.000001, 0.00002],
                 [0.00002,  0.0004]])
@@ -47,41 +45,41 @@ AT = A.T
 CT = C.T
 
 
-Y = dataDF.data.tolist() # Kalman filter input data, gotten from the data.csv file, as a list
-X = [np.matrix([[0], [0]])] # State estimation of the Kalman filter, as a list, with inital position and speed set to 0
+Y = dataDF.data.tolist() # Kalman filter input data, gotten from the data.csv file, as a list of floating points
+X = [np.matrix([[0], [0]])] # State estimation of the Kalman filter, as a list of matrixes, with inital position and speed set to 0
 
 
 Pk = np.matrix([[0, 0],
-                [0, 0]]) # Inital Estimation error covariance
+                [0, 0]]) # Inital Estimation error covariance, used in the forloop to keep track of the previous calculation
 
 positionResultList = [0] # A list to contain the position calculated by the KF Filter
 
 
 # The Kalman Filter
 for k in range(0, len(Y) - 1):
+    
+    Xk = X[k] # Previsous calcluated output
+    Ykplus1 = Y[k + 1] # Input
 
-    Xk = X[k]
-    Ykplus1 = Y[k + 1]
-
-    K = A * Pk * CT * (C * Pk @ CT + Sz)**-1 # Kalman Gain
+    K = A * Pk * CT * (C * Pk * CT + Sz)**-1 # Kalman Gain
 
     Xkplus1 = (A * Xk + B * uk) + K * (Ykplus1 - C * Xk) # State estimation
 
     Pkplus1 = A * Pk * AT + Sw - A * Pk * CT * Sz**-1 * C * Pk * AT # Estimation error covariance
 
-    X.append(Xkplus1)
-    positionResultList.append(Xkplus1.item(0))
+    X.append(Xkplus1) # Add the newly calculated output to the list
+    positionResultList.append(Xkplus1.item(0)) # Add the position results that where interested in to the list
 
-    Pk = Pkplus1
+    Pk = Pkplus1 # Set the newly calculated Pk to the Pk variable so we can use it in the next loop cycle
 
 
 # Add the calculated positions to the dataframe
 dataDF['calc_kf'] = positionResultList
 
-# Calculate the differnce between given and calculated filter and save to dataframe
+# Calculate the absolute difference between given and calculated filter and save to dataframe
 dataDF['diff_given_calc'] = abs(dataDF['given_kf'] - dataDF['calc_kf'])
 
-# Save the dataframe to a CSV file
+# Save the dataframe to the result CSV file
 dataDF.to_csv(resultsFilePath)
 
 
@@ -91,7 +89,7 @@ fig, axs = plt.subplots(2, 2)
 dataDF['data'].plot(ax=axs[0,0], title="Original data", legend=True)
 dataDF['given_kf'].plot(ax=axs[0,1], title="Given Kalman Filter", legend=True)
 dataDF['calc_kf'].plot(ax=axs[1,0], title="Calculated Kalman Filter", legend=True)
-dataDF['diff_given_calc'].plot(ax=axs[1,1], title="Differnce between given and calculated filter", legend=True)
+dataDF['diff_given_calc'].plot(ax=axs[1,1], title="Absolute difference between given and calculated filter", legend=True)
 
 plt.show()
 
