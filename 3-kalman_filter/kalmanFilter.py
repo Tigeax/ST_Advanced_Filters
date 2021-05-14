dffb03fc3d87e7e06284dfe972b9463563e3a117
Jesse Braaksma <jessebraaksma@gmail.com>
2021-05-14 13:28:05 +0200

    Updated assignment 3

--- 3-kalman_filter/kalmanFilter.py
+++ 3-kalman_filter/kalmanFilter.py
@@ -16,7 +16,7 @@ dataFilePath = "3-kalman_filter/data.csv"
 resultsFilePath = "3-kalman_filter/results.csv"
 
 # Get the simulated data from the csv file as a pandas dataframe
-dataDF = pd.read_csv(dataFilePath, names=['gyro_roll', 'gyro_pitch', 'gyro_yaw', 'accel_x', 'accel_y', 'accel_z'], header=1)
+dataDF = pd.read_csv(dataFilePath, names=['gyro_roll', 'gyro_pitch', 'gyro_yaw', 'accel_x', 'accel_y', 'accel_z'], header=0)
 
 T = 0.01
 g = 9.81 # Grafity
@@ -40,7 +40,7 @@ yaw_arr = []
 
 
 # The Kalman Filter
-for k in range(0, len(dataDF)):
+for k in range(len(dataDF)):
 
     # Previous calculated output
     Xk = X[k]
@@ -50,6 +50,14 @@ for k in range(0, len(dataDF)):
     q = dataDF.gyro_pitch.at[k]
     r = dataDF.gyro_yaw.at[k]
 
+    # Calculate matrix A
+    A = H + (T / 2) * np.matrix([
+                                    [0, -p, -q, -r],
+                                    [p,  0,  r, -q],
+                                    [q, -r,  0,  p],
+                                    [r,  q, -p,  0]
+                                ])
+
     # Measured accelerations
     accelX = dataDF.accel_x.at[k]
     accelY = dataDF.accel_y.at[k]
@@ -57,7 +65,7 @@ for k in range(0, len(dataDF)):
 
     O = np.arcsin(accelX / g)                      # Calculate pitch
     Pi = np.arcsin(-(accelY) / (g  * (np.cos(O)))) # Calculate roll
-    W = 0                                               # Yaw is always 0
+    W = 0                                          # Yaw is always 0
 
      # Calculate the quaturnions
     quat = np.matrix([
@@ -67,14 +75,6 @@ for k in range(0, len(dataDF)):
         [(np.cos(Pi/2) * np.cos(O/2) * np.sin(W/2)) - (np.sin(Pi/2) * np.sin(O/2) * np.cos(W/2))  ]
         ])
 
-
-    # Calculate matrix A
-    A = H + (T / 2) * np.matrix([
-                                    [0, -p, -q, -r],
-                                    [p,  0,  r, -q],
-                                    [q, -r,  0,  p],
-                                    [r,  q, -p,  0]
-                                ])
 
     Pk = A * Pk * A.T + Q
 
@@ -88,16 +88,16 @@ for k in range(0, len(dataDF)):
     Pk = Pkplus1
 
 
-    #Get output quaternions from prediction results
+    # Get output quaternions from prediction results
     q0 = XkPlus1.item(0)
     q1 = XkPlus1.item(1)
     q2 = XkPlus1.item(2)
     q3 = XkPlus1.item(3)
     
-    #Quaternions to roll/pitch/yaw
-    roll = np.arctan2((2*(q0*q1+q2*q3)),(1-2*(q1**2+q2**2)))
-    pitch = np.arcsin(2*(q0*q2-q3*q1))
-    yaw = np.arctan2((2*(q0*q3+q1*q2)),(1-2*(q2**2+q3**2)))
+    # Quaternions to roll/pitch/yaw
+    roll = np.arctan2((2 * (q0 * q1 + q2 * q3)), (1 - 2 * (q1**2 + q2**2)))
+    pitch = np.arcsin(2 * (q0 * q2 - q3 * q1))
+    yaw = np.arctan2((2 * (q0 * q3 + q1 * q2)), (1 - 2*(q2**2 + q3**2)))
     
     roll_arr = np.append(roll_arr, roll)
     pitch_arr = np.append(pitch_arr, pitch)
@@ -109,5 +109,8 @@ dataDF['pitch'] = pitch_arr
 dataDF['yaw'] = yaw_arr
 
 
-plt.plot(dataDF['yaw'])
+fig, axs = plt.subplots(2, 2)
+dataDF['roll'].plot(ax=axs[0,0], title="Roll", legend=True)
+dataDF['pitch'].plot(ax=axs[0,1], title="Pitch", legend=True)
+dataDF['yaw'].plot(ax=axs[1,0], title="Yaw", legend=True)
 plt.show()
