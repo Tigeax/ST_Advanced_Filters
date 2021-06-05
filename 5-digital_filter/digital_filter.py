163508af9112f4e5353aeec55254a32260513a56
Jesse Braaksma <jessebraaksma@gmail.com>
2021-06-05 16:13:13 +0200

    Finished digital filter

--- 5-digital_filter/digital_filter.py
+++ 5-digital_filter/digital_filter.py
@@ -4,69 +4,79 @@ Electrical and Electronic Engineering - Major Sensor Technolgyy
 Advanced Filters
 Assignment #5 - Digital Filter
 By Jesse Braaksma
-22/05/2021
+05/06/2021
 """
 
 import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from numpy import pi
-from cmath import phase
 
 
-samplingFreq = 600/3
+# The maximum frequency that we want to keep in the signal
+lowpassFilterFreq = 40
 
+
+# Read the data from the file
 dataFilePath = "5-digital_filter/data.csv"
 dataDF = pd.read_csv(dataFilePath, names=['index', 'time', 'signal'], header=0)
-signal = dataDF['signal'].tolist()
+
+# Get the signal as a numpy list
+signal = dataDF['signal'].to_numpy()
+
+# Get the number of samples
+numSamples = dataDF['index'].max()
+
+# Get the sample time
+sampleTime = dataDF['time'].max()
+
+# Calculate the sampling frequency
+samplingFreq = numSamples/sampleTime
+print(f"Sampling frequency: {samplingFreq}Hz")
 
 
+# Get the frequencies to display on the x-axis
+freqs = samplingFreq * (np.linspace(-0.5, 0.5, len(signal)))
 
-def DFT(samples):
-    ''' Take the Discrete Fourier Transform of a signal, where the signal is a list of values, return as complex narray'''
+# Get the DFT
+dft = np.fft.fft(signal)
 
-    numSamples = len(samples)
-    frequencyResponse = np.array([]) # Numpy array to hold complex numbers
+# Shift the DFT
+dftShifted = np.fft.fftshift(dft)
 
-    # For each frequency (0 to numSamples - 1)
-    for k in range(numSamples): # We don't have to use -1 since range does not include the final number of numSamples
+# Filter the shifted DFT, by setting all values where the frequency is higher then our lowpass frequency to 0
+dftShiftedLowpass = dftShifted.copy()
+dftShiftedLowpass[(abs(freqs) > lowpassFilterFreq)] = 0
 
-        sumRe = 0 # Sum of the real part
-        sumIm = 0 # Sum of the imaginary part
+# Inverse shift the shifted DFT
+dftLowpass = np.fft.ifftshift(dftShiftedLowpass)
 
-        # Sum all samples
-        for n in range(numSamples):
-            u = -2 * pi * k * n / numSamples
-            yRe = samples[n] * np.cos(u) # Real part of frequency response the sample
-            yIm = samples[n] * np.sin(u) # Imaginary part of frequency response the sample
+# Get the inverse of the DFT to get our filtered sigal
+signalFiltered = np.fft.ifft(dftLowpass)
 
-            sumRe += yRe
-            sumIm += yIm
+# Get the DFT's magnitude's (abs() on a complex number returns it's magnitude)
+dftShiftedMag = abs(dftShifted)
+dftShiftedLowpassMag = abs(dftShiftedLowpass)
 
-        z = complex(sumRe, sumIm)
-        frequencyResponse = np.append(frequencyResponse, z)
-
-    return frequencyResponse
-
-
-def calcComplexNumMagnitude(z):
-    ''' Return the magnitude of a complex number. '''
-    magnitude = np.sqrt(np.square(z.real) + np.square(z.imag))
-    return magnitude
-
-
-dft = DFT(signal)
-
-freqAxis= samplingFreq * (np.linspace(-0.5, 0.5, len(signal)))
-
-dftMag = abs(dft) # Taking the absolute value of a complex number return's it's magitude
-dftShift = np.fft.fftshift(dftMag)
-dtfPhase = [phase(z) for z in dft]
 
 # Plot the results
 fig, axs = plt.subplots(2, 2)
-axs[0,0].plot(signal)
-axs[0,1].plot(freqAxis, dftShift)
-axs[1,0].plot(freqAxis, dtfPhase)
+fig.tight_layout()
+
+axs[0,0].set_title('Signal')
+axs[0,0].set(xlabel='time (s)')
+axs[0,0].plot(dataDF['time'], signal)
+
+axs[0,1].set_title('DFT')
+axs[0,1].set(xlabel='Frequency (Hz)', ylabel='Magnitude')
+axs[0,1].plot(freqs, dftShiftedMag)
+
+axs[1,0].set_title('Filtered DFT')
+axs[1,0].set(xlabel='Frequency (Hz)', ylabel='Magnitude')
+axs[1,0].plot(freqs, dftShiftedLowpassMag)
+
+axs[1,1].set_title('Filtered signal')
+axs[1,1].set(xlabel='time (s)')
+axs[1,1].plot(dataDF['time'], signalFiltered)
+
 plt.show()
